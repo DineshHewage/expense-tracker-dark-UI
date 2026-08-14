137872fa15b957411057d1492bb2919a0aa1abe2
DineshHewage <hewage.d.sampath@gmail.com>
2026-08-14 10:50:57 +0530

    UI Improvement(Add edit button) + Display information of clicked item on the input boxes

--- script.js
+++ script.js
@@ -47,12 +47,24 @@ function renderExpense() {
     const span = document.createElement("span");
     span.textContent = `${element.name} - Rs ${element.amount.toFixed(2)}`;
 
+    const editBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
+    editBtn.classList.add("edit-btn");
+    editBtn.dataset.id = element.id;
+
     const deteteBtn = document.createElement("button");
     deteteBtn.textContent = "Delete";
+    deteteBtn.classList.add("delete-btn");
     deteteBtn.dataset.id = element.id;
 
+    // Wrap both button
+    const btnGroup = document.createElement("div");
+    btnGroup.classList.add("btn-group");
+
     li.appendChild(span);
-    li.appendChild(deteteBtn);
+    btnGroup.appendChild(editBtn);
+    btnGroup.appendChild(deteteBtn);
+    li.appendChild(btnGroup);
     expenseList.appendChild(li);
   });
 }
@@ -76,17 +88,28 @@ function saveExpenseToLocal() {
 }
 calculateTotal();
 
-// Remove expense
+// Remove & edit expense
 expenseList.addEventListener("click", (e) => {
-  if (e.target.tagName === "BUTTON") {
+  // Delete button click
+  if (e.target.classList.contains("delete-btn")) {
     // Ask user to reconfim the delete request.
-    const isConfirmed = confirm("Do you want to delete this expense?.");
-    if (isConfirmed) {
-      const id = e.target.dataset.id;
-      expenses = expenses.filter((ex) => ex.id !== Number(id));
+    const isConfirmedDelete = confirm("Do you want to delete this expense?.");
+    if (isConfirmedDelete) {
+      const idDel = e.target.dataset.id;
+      expenses = expenses.filter((ex) => ex.id !== Number(idDel));
       saveExpenseToLocal();
       renderExpense();
       calculateTotal();
+    }
+  }
+  // Edit button click
+  if (e.target.classList.contains("edit-btn")) {
+    const isConfirmedEdit = confirm("Do you want to edit the expense");
+    if (isConfirmedEdit) {
+      const idEdit = e.target.dataset.id;
+      const expense = expenses.find((ex) => ex.id === Number(idEdit));
+      expenseNameInput.value = expense.name;
+      expenseAmountInput.value = expense.amount;
     }
   }
 });
